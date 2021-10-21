4e73323289c2d07318ab37f80d90bce32d1f2593
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-10-21 15:19:39 -0300

    SmtpEmailService com servidor do Google

--- src/main/java/com/luxfacta/desafio/config/DevConfig.java
+++ src/main/java/com/luxfacta/desafio/config/DevConfig.java
@@ -1,6 +1,8 @@
 package com.luxfacta.desafio.config;
 
 import com.luxfacta.desafio.services.DBService;
+import com.luxfacta.desafio.services.EmailService;
+import com.luxfacta.desafio.services.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -27,5 +29,10 @@ public class DevConfig {
         dbService.instantiateTestDataBase();
 
         return true;
+    }
+
+    @Bean
+    public EmailService emailService() {
+        return new SmtpEmailService();
     }
 }
